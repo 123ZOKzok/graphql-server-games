6a8c5e4dd1d16cbfb0835d4e19b1ec42b7a283f2
123ZOKzok <zippyondiba@gmail.com>
2023-11-26 18:16:45 +0300

    add type definitions

--- scheme.js
+++ scheme.js
@@ -1,9 +1,24 @@
 export const typeDefs = `#graphql
-
-
-
-
-
+  type Game {
+    id: ID!,
+    title: string!,
+    platform:[string!]!
+}
+type Review{
+    id: ID!,
+    rating: Int!,
+    content: string!
+}
+type Author{
+    id: ID!,
+    name: string!,
+    verified: Boolean!
+}
+type Query{
+    reviews: [Reviews]
+    games: [Game]
+    authors: [Author]
+}
 
 
 `
